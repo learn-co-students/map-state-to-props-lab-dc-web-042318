dc2ef257e032b0f9780c1688ec449abbece2b2fe
munirkizilcali <munir.kizilcali@gmail.com>
2018-07-12 16:19:36 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export class Users extends Component {
-
-  render() {
-
-    return (
-      <div>
-        <ul>
-          {/* stuff should happen around here */}
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<ul>
+					{this.props.users.map(user => {
+						return (
+							<li key={user.userName}>
+								{user.userName} - {user.hometown}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
-export const ConnectedUsers = Users // aren't we supposed to be connecting something around here?
+const mapStateToProps = state => {
+	return {
+		users: state.users,
+		primaryUser: state.users[0]
+	};
+};
 
+export const ConnectedUsers = connect(mapStateToProps)(Users); // aren't we supposed to be connecting something around here?
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,14 +1,21 @@
-export default function manageUsers(state = {
-  users: [],
-}, action){
-  switch (action.type) {
-
-    case 'ADD_USER':
-      return Object.assign({}, state, {
-        users: state.users.concat(action.user)
-      });
-
-    default:
-      return state;
-  }
-};
+export default function manageUsers(
+	state = {
+		users: [],
+		primaryUser: ""
+	},
+	action
+) {
+	switch (action.type) {
+		case "ADD_USER":
+			// let primaryUser = null;
+			// state.primaryUser !== ""
+			// 	? (primaryUser = state.primaryUser)
+			// 	: (primaryUser = action.user);
+			return Object.assign({}, state, {
+				users: state.users.concat(action.user)
+				// primaryUser
+			});
+		default:
+			return state;
+	}
+}
